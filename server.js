30466d359fe53b00eea9b9b7dcfb901ca3f94641
jon_prog <jonashenri.aquino@gmail.com>
2023-12-27 01:13:46 -0300

    add:webpack

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const path = require('path');
 
 
 app.use(express.urlencoded({extended:true}));
-app.set(express.static(path.resolve(__dirname,'public')))
+app.use(express.static(path.resolve(__dirname,'public')));
+
+/*
+A pasta public é automaticamente colocada no diretorio de pastas do servidor
+fazendo com que pareça que as views estão dentro da pasta public facilitando
+na hora de colocar os caminhos dos aquivos estáticos
+*/
 
 app.set('views', path.resolve(__dirname,'src','views'));
 app.set('view engine','ejs'); //Serve para adicinar a logica do js no html
 
 app.use(routes);
-
 
 app.listen(3000,()=>{
     console.log('Acess http://localhost:3000');
